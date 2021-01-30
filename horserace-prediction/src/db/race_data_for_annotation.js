57036c3c7dab9e7fc66a290eff4493fdab831e0f
Ubuntu <ubuntu@ip-172-31-21-45.ap-northeast-1.compute.internal>
2021-01-30 13:58:43 +0000

    bug fix

--- horserace-prediction/src/db/race_data_for_annotation.js
+++ horserace-prediction/src/db/race_data_for_annotation.js
@@ -14,14 +14,27 @@ const selectId = async function(con){
 const selectLatestData = async function(con){
     con.query = util.promisify(con.query);
     // trackbias_XXXのデフォルト値が0
-    // とりあえず芝固定。。。できない。。。。。
-    let sql = "SELECT * FROM race_data_for_annotation WHERE (trackbias_course = 0 OR trackbias_racestyle = 0) ORDER BY id ASC LIMIT 1";
+    // とりあえず芝固定。。。できない。。。。。ダブルクォートが
+    let sql = "SELECT * FROM race_data_for_annotation WHERE course = '\"芝\"' AND date > 20150000 AND (trackbias_course = 0 OR trackbias_racestyle = 0) ORDER BY id ASC LIMIT 1";
     // ユーザ入力のデータをSQLに使用しないため直
     // let select = ['race_data_for_annotation', 'id', 5];
     // sql = await mysql.format(sql, select);
     let res = await con.query(sql);
+    console.log(res)
+    console.log(sql)
     con.end();
     return res;
 };
 
-module.exports = {'selectId': selectId, 'selectLatestData': selectLatestData };
+const updateBiasData = async function (id, biasCourse, biasStyle, con) {
+    con.query = util.promisify(con.query);
+    let sql = "UPDATE race_data_for_annotation SET trackbias_racestyle = ?, trackbias_course = ? WHERE id = ?";
+    let select = [biasStyle,　biasCourse, id];
+    sql = await mysql.format(sql, select);
+    console.log(sql)
+    let res = await con.query(sql);
+    con.end();
+    return res;
+}
+
+module.exports = {'selectId': selectId, 'selectLatestData': selectLatestData, 'updateBiasData': updateBiasData };
